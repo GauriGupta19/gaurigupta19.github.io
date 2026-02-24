4d99cbf08a97ec53765f5b9c91aa5b8d47fc6daa
GitHub Action <action@github.com>
2026-02-24 19:37:33 +0000

    chore: add blog posts from X bookmarks [skip ci]

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -1,7 +1,11 @@
 import llmOptimizationMd from '../pages/posts/llm-optimization.md?raw';
+import x_bookmark_2023878690950836596Md from '../pages/posts/x-bookmark-2023878690950836596.md?raw';
+import x_bookmark_1991824700910235973Md from '../pages/posts/x-bookmark-1991824700910235973.md?raw';
+import x_bookmark_1994928193527320725Md from '../pages/posts/x-bookmark-1994928193527320725.md?raw';
+
 
 const posts = [
-  {
+{
     id: 'llm-optimization',
     title: 'LLM Optimization Notes: Memory, Compute & Inference Techniques',
     date: 'October 2025',
@@ -10,6 +14,33 @@ const posts = [
     excerpt:
       'These are my job preparation notes on optimizing the training and inference of large language models. This covers a range of techniques to improve memory usage, computational efficiency, and inference optimization techniques along with advanced parallelism strategies used in LLMs.',
     content: llmOptimizationMd,
+  },
+  {
+    id: 'x-bookmark-2023878690950836596',
+    title: "Super excited and honoured to be a part of Delta Fellows along with such incredibly talented folks! ",
+    date: 'February 2026',
+    author: "Gauri Gupta",
+    categories: ["X Bookmark","Curated"],
+    excerpt: "Super excited and honoured to be a part of Delta Fellows along with such incredibly talented folks! See y’all soon :) https://t.co/CiXBLeWNnK",
+    content: x_bookmark_2023878690950836596Md,
+  },
+  {
+    id: 'x-bookmark-1991824700910235973',
+    title: "Honored to be invited to the OpenAI Founders Table dinner. Incredible night with great people and am",
+    date: 'November 2025',
+    author: "Gauri Gupta",
+    categories: ["X Bookmark","Curated"],
+    excerpt: "Honored to be invited to the OpenAI Founders Table dinner. Incredible night with great people and amazing conversations.   Excited for what we are building @NeoSigmaAI https://t.co/faoyoXjXPd",
+    content: x_bookmark_1991824700910235973Md,
+  },
+  {
+    id: 'x-bookmark-1994928193527320725',
+    title: "Last month I had the privilege of presenting at the MIT AI Conference alongside incredible leaders a",
+    date: 'November 2025',
+    author: "Gauri Gupta",
+    categories: ["X Bookmark","Curated"],
+    excerpt: "Last month I had the privilege of presenting at the MIT AI Conference alongside incredible leaders and researchers. I shared what we are building at @NeoSigmaAI  on curating intelligence in the post-w...",
+    content: x_bookmark_1994928193527320725Md,
   }
 ];
 
