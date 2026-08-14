904a895b2fa588c72a4e6646ba2a4af1bf467be7
GitHub Action <action@github.com>
2026-08-14 17:51:36 +0000

    chore: add X bookmarks to news

--- src/data/news.ts
+++ src/data/news.ts
@@ -1,5 +1,13 @@
 export const news = [
   {
+    date: "August 2026",
+    content: "<a href='https://x.com/ritvikkapila/status/2087964978263584957' target='_blank' rel='noreferrer'>When a new, capable model is ported to a stale harness, we notice a loss in performance even with a smarter model. This is what we call \"The Scaffolding Tax\". Check out our most recent blog: https:// x.com/neosigma_ai/st atus/2087962887482700209?s=20 … Great work by @SChen1249 from our team!</a>",
+  },
+  {
+    date: "August 2026",
+    content: "<a href='https://x.com/neosigma_ai/status/2087962887482700209' target='_blank' rel='noreferrer'></a>",
+  },
+{
     date: "January 2024",
     content: "<a href='https://x.com/eladgil/status/1745851731593101641' target='_blank' rel='noreferrer'>Things to do when asking for an intro: -Start a fresh clean email with a relevant subject line someone can forward easily. Dont reply with a bunch of crap inline alongside other musings, questions etc -Explain who you are and why you are interesting/special. Drop some brand</a>",
   },
